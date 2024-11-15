2236faa3101af1d83969dbdc3ceaab47f83feea8
Derroce <arthur.descourvieres@gmail.com>
2024-11-15 11:17:17 +0100

    removed el

--- jour01/job04/script.js
+++ jour01/job04/script.js
@@ -1,7 +1,5 @@
 function bisextile(annee) {
-    // Une année est bisextile si elle est divisible par 4
-    // Sauf si elle est divisible par 100
-    // Auquel cas, elle doit aussi être divisible par 400 pour être bisextile
+
     if ((annee % 4 === 0 && annee % 100 !== 0) || (annee % 400 === 0)) {
         return true;
     } else {
