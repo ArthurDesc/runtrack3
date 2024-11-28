2c0139cbca0ea75c893e3332c0ac8679ad36b42e
Arthur <162974272+ArthurDesc@users.noreply.github.com>
2024-11-28 10:49:16 +0100

    removed calendar

--- calendar/exportData.js
+++
@@ -1,5 +0,0 @@
-const { exportDataToDatabase } = require('./database');
-
-exportDataToDatabase()
-  .then(() => console.log('Exportation terminée'))
-  .catch(error => console.error('Erreur lors de l\'exportation:', error));
